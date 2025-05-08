4dcff1c6ac8b9d0755591402441276fd153c0db0
josefk-eng <josefkigozi@gmail.com>
2025-05-08 06:48:48 +0300

    removed basepath

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
   images: { unoptimized: true }, // <=== disables image optimization
-  basePath: "/seagate-electric", // <=== sets the base path for static exports
+  // basePath: "/seagate-electric", // <=== sets the base path for static exports
   async redirects() {
     return [
       {
